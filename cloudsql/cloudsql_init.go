a13aa5a844ae805190e071af46dc12b0d3d2b1ab
zhoujiang@codoon.com <zhoujiang@codoon.com>
2023-10-23 14:13:52 +0800

    v1.1

--- cloudsql/cloudsql_init.go
+++ cloudsql/cloudsql_init.go
@@ -2,7 +2,6 @@ package cloudsql
 
 import (
 	"database/sql"
-	"git.in.codoon.com/Overseas/runbox/first-test/model"
 	"log"
 	"os"
 	"sync"
@@ -43,6 +42,29 @@ func migrateGpsTable(db *sql.DB) error {
 		id SERIAL NOT NULL,
 		user_id datetime NOT NULL,
 		total_length int NOT NULL,
+		PRIMARY KEY (id)
+	);`
+	_, err := db.Exec(createGpsData)
+	return err
+}
+
+func MigrateGpsRouteTable(db *sql.DB) error {
+	createGpsData := `CREATE TABLE IF NOT EXISTS gps_route_data (
+		id SERIAL NOT NULL,
+		route_id VARCHAR(100) NOT NULL,
+		user_id  VARCHAR(100) NOT NULL,
+		total_length int NOT NULL,
+   		 total_time data NOT NULL,
+        total_calories int NOT NULL,
+        location VARCHAR(100)  NULL,
+     sports_type int NOT NULL,
+     start_time datetime NOT NULL,
+     EndTime datetime NOT NULL,
+     upload_time datetime NOT NULL,
+     locus_url varchar(100) NULL,
+     locus_url2 varchar(100)  NULL,
+     steps int  NULL,
+     file_url VARCHAR(200) NOT NULL,
 		PRIMARY KEY (id)
 	);`
 	_, err := db.Exec(createGpsData)
@@ -99,7 +121,7 @@ func mustConnect() *sql.DB {
 	}
 
 	log.Printf("create table,create table,create table")
-	if err := model.MigrateGpsRouteTable(db); err != nil {
+	if err := MigrateGpsRouteTable(db); err != nil {
 		log.Fatalf("MigrateGpsRouteTable to create table: %s", err)
 	}
 
